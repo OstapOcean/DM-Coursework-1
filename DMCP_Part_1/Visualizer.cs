51e62e2327ab61e4b15282b9711da833e8164882
Aleksej <algameacc@gmail.com>
2016-11-12 20:50:53 +0200

    4

---
+++ DMCP_Part_1/Visualizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using QuickGraph;
+
+namespace DMCP_Part_1
+{
+  
+    class Visualizer : INotifyPropertyChanged
+    {
+        IBidirectionalGraph<GVertex, GEdge> graphToShow;
+        public Visualizer()
+        {
+
+        }
+
+    }
+}
