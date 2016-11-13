0c039711d18c254c40cc13ac84b27253dddbcfe5
OstapOcean <ostapocean@gmail.com>
2016-11-13 12:24:37 +0200

    Добавил окно ввода данных

---
+++ DMCP_Part_1/Controllers/InputController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+
+namespace DMCP_Part_1.Controllers {
+	class InputController : INotifyPropertyChanged {
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged (string propertyChanged) {
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyChanged));
+		}
+
+		private int providersCount;
+		public int ProvidersCount {
+			get { return providersCount; }
+			set {
+				providersCount = value;
+				OnPropertyChanged("ProvidersCount");
+			}
+		}
+
+		private int receiversCount;
+		public int ReceiversCount {
+			get { return receiversCount; }
+			set {
+				receiversCount = value;
+				OnPropertyChanged("ProvidersCount");
+			}
+		}
+
+		private List<int> providers;
+		public List<int> Providers {
+			get { return providers; }
+			set {
+				providers = value;
+				OnPropertyChanged("Providers");
+			}
+		}
+
+		private List<int> receivers;
+		public List<int> Receivers {
+			get { return receivers; }
+			set {
+				receivers = value;
+				OnPropertyChanged("Receivers");
+			}
+		}
+
+		private Delegate createLists_delegate;
+		public Delegate CreateLists_delegate {
+			get { return createLists_delegate ?? (createLists_delegate = new Delegate(CreateLists)); }
+		}
+		public void CreateLists (object args) {
+			Providers = new List<int>();
+			for (int i = 0; i < ProvidersCount; i++)
+				Providers.Add(i);
+
+			Receivers = new List<int>();
+			for (int i = 0; i < ReceiversCount; i++)
+				Receivers.Add(i);
+		}
+
+	}
+}
---
+++ DMCP_Part_1/InputWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using DMCP_Part_1.Controllers;
+
+namespace DMCP_Part_1 {
+	/// <summary>
+	/// Логика взаимодействия для InputWindow.xaml
+	/// </summary>
+	public partial class InputWindow : Window {
+
+		private readonly InputController controller;
+
+		public InputWindow () {
+			InitializeComponent();
+
+			controller = new InputController();
+			DataContext = controller;
+		}
+
+		private void textBox_TextChanged (object sender, TextChangedEventArgs e) { }
+	}
+}
