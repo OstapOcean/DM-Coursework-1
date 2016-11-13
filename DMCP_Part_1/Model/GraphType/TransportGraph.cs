0cd2d12619c0f252a949fa74192d6668b688eb18
Aleksej <algameacc@gmail.com>
2016-11-13 12:13:18 +0200

    Final_Upload

---
+++ DMCP_Part_1/Model/GraphType/TransportGraph.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickGraph;
+
+
+namespace DMCP_Part_1
+{
+    public class TransportGraph : BidirectionalGraph<GVertex, GEdge>
+    {
+        private int _flow;
+        public int Flow
+        {
+            get { return _flow; }
+        }
+        private int _deltaFlow;
+        public int DeltaFlow
+        {
+            get { return _deltaFlow; }
+        }
+        public TransportGraph(int flow, int deltaFlow)
+        {
+            _flow = flow;
+            _deltaFlow = deltaFlow;
+        }
+    }
+}
